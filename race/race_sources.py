e5550fe0cc675096490a5bf0aaf6650d6d85fd92
Edirne22 <bduelger@tutanota.de>
2026-09-12 15:27:26 +0200

    Agents: Inspiration, Race und Viral Grundlagen anlegen

---
+++ race/race_sources.py
@@ -0,0 +1,2 @@
+"""Ausschließlich öffentliche Quellen für Rennkalender."""
+SOURCES={"MotoGP":["https://www.motogp.com/","https://www.motorsport-magazin.com/motogp/"],"WorldSBK":["https://www.worldsbk.com/","https://www.servustv.com/sport/"],"Formel 1":["https://www.formula1.com/","https://www.allgaeuer-zeitung.de/sport/"]}
