7bda1f4aad42149b6618a37aee953150383b4eb7
Edirne22 <bduelger@tutanota.de>
2026-09-26 23:06:46 +0200

    test: cover MotoEtkinlik rider discovery sources

---
+++ tests/test_turkish_media_sources.py
@@ -0,0 +1,14 @@
+import turkish_riders_scout as scout
+import motogp_content_agency_v2 as agency
+
+urls=dict(scout.TURKISH_MEDIA_SOURCES)
+assert urls['MotoEtkinlikcom']=='https://www.instagram.com/motoetkinlikcom/'
+assert urls['MotoEtkinlikRacing']=='https://www.instagram.com/motoetkinlikracing/'
+for text,expected in [
+ ('Toprak Razgatlioglu yeni haber','Toprak Razgatlıoğlu'),
+ ('Can Oncu WorldSSP','Can Öncü'),
+ ('Deniz Oncu Moto2','Deniz Öncü'),
+]:
+ assert scout.rider_for(text)==expected,(text,scout.rider_for(text))
+ assert agency.detect_turkish_rider({'title':text,'summary':'','url':''})==expected
+print('TEST – Turkish Media Sources: PASS')
